2670fd554e8c33c73053be229aeb43ade92b5c1c
Eike von Tils <eike.vontils@informatik.uni-luebeck.de>
2014-03-13 10:43:49 +0100

    Adds basic AngularJS
    Adds simple controllers for mobile and desktop view in app.js
    Adds static testing data in rootScope in app.js
    
    Refs #1

---
+++ app/js/app.js
@@ -0,0 +1,40 @@
+
+function DesktopViewCtrl($rootScope) {
+	$rootScope.weekdays = [
+		'Montag','Dienstag','Mittwoch','Donnerstag','Freitag'
+	];
+	
+	$rootScope.meals = [
+		{ text:'Menü 1', id:0 },
+		{ text:'Menü 2', id:1 }
+	];
+	
+	$rootScope.days = [
+		[
+			{ text:'Spaghetti', price:'2,30', id:0 },
+			{ text:'Fleisch', price:'3,00', id:1 }
+		],
+		[
+			{ text:'Fisch', price:'4,30', id:0 },
+			{ text:'Salat', price:'2,00', id:1 }
+		],
+		[
+			{ text:'Geschnetzeltes', price:'3,30', id:0 },
+			{ text:'Nudeln', price:'1,70', id:1 }
+		],
+		[
+			{ text:'Braten', price:'4,10', id:0 },
+			{ text:'Pizza', price:'3,00', id:1 }
+		],
+		[
+			{ text:'Milchreis', price:'2,00', id:0 },
+			{ text:'Currywurst', price:'4,00', id:1 }
+		]
+	];
+		
+}
+
+function MobileViewCtrl($rootScope) {
+	$rootScope.currentDay = 1; //new Date().getDay(); //todo
+	
+}

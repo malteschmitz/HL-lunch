918e9a728aa3f6636739f79df67888c72a7d7886
Eike von Tils <eike.vontils@informatik.uni-luebeck.de>
2014-03-13 14:12:56 +0100

    Refactors app into modules
    refs #2

--- app/js/app.js
+++ app/js/app.js
@@ -1,40 +1,6 @@
 
-function DesktopViewCtrl($rootScope, $scope) {
-	$rootScope.weekdays = [
-		'Montag','Dienstag','Mittwoch','Donnerstag','Freitag'
-	];
-	
-	$rootScope.meals = [
-		{ text:'Menü 1', id:0 },
-		{ text:'Menü 2', id:1 }
-	];
-	
-	$rootScope.days = [
-		[
-			{ text:'Spaghetti', price:'2,30', id:0 },
-			{ text:'Fleisch', price:'3,00', id:1 }
-		],
-		[
-			{ text:'Fisch', price:'4,30', id:0 },
-			{ text:'Salat', price:'2,00', id:1 }
-		],
-		[
-			{ text:'Geschnetzeltes', price:'3,30', id:0 },
-			{ text:'Nudeln', price:'1,70', id:1 }
-		],
-		[
-			{ text:'Braten', price:'4,10', id:0 },
-			{ text:'Pizza', price:'3,00', id:1 }
-		],
-		[
-			{ text:'Milchreis', price:'2,00', id:0 },
-			{ text:'Currywurst', price:'4,00', id:1 }
-		]
-	];
-	
-	
-}
+var lunchApp = angular.module('lunchApp', [
+		'lunchControllers'
+	]
+);
 
-function MobileViewCtrl($rootScope) {
-	$rootScope.currentDay = new Date().getDay() - 1;
-}
---
+++ app/js/controllers.js
@@ -0,0 +1,52 @@
+
+var lunchControllers = angular.module('lunchControllers', []);
+
+lunchControllers.controller('MobileViewCtrl', ['$rootScope', 
+	function($rootScope) {
+
+// legacy code
+		$rootScope.weekdays = [
+		'Montag','Dienstag','Mittwoch','Donnerstag','Freitag'
+	];
+	
+	$rootScope.meals = [
+		{ text:'Menü 1', id:0 },
+		{ text:'Menü 2', id:1 }
+	];
+	
+	$rootScope.days = [
+		[
+			{ text:'Spaghetti', price:'2,30', id:0 },
+			{ text:'Fleisch', price:'3,00', id:1 }
+		],
+		[
+			{ text:'Fisch', price:'4,30', id:0 },
+			{ text:'Salat', price:'2,00', id:1 }
+		],
+		[
+			{ text:'Geschnetzeltes', price:'3,30', id:0 },
+			{ text:'Nudeln', price:'1,70', id:1 }
+		],
+		[
+			{ text:'Braten', price:'4,10', id:0 },
+			{ text:'Pizza', price:'3,00', id:1 }
+		],
+		[
+			{ text:'Milchreis', price:'2,00', id:0 },
+			{ text:'Currywurst', price:'4,00', id:1 }
+		]
+	];
+// end of legacy code
+
+	}
+]);
+
+lunchControllers.controller('DesktopViewCtrl', ['$rootScope',
+	function($rootScope) {
+		
+// legacy code
+		$rootScope.currentDay = new Date().getDay() - 1;
+// end of legacy code
+		
+	}
+]);
